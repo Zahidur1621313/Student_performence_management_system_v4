5e41b022cba78f8768025487545b87d024e255b3
ZahidurRahman1621313 <1621313@iub.edu.bd>
2022-12-07 00:35:12 +0600

    first commit

---
+++ student_management_app/views.py
@@ -0,0 +1,93 @@
+# from channels.auth import login, logout
+from django.contrib.auth import authenticate, login, logout
+from django.http import HttpResponseRedirect, HttpResponse
+from django.shortcuts import render, redirect
+from django.contrib import messages
+
+from student_management_app.EmailBackEnd import EmailBackEnd
+
+
+def home(request):
+    return render(request, 'index.html')
+
+
+def loginPage(request):
+    return render(request, 'login.html')
+
+
+
+def doLogin(request):
+    if request.method != "POST":
+        return HttpResponse("<h2>Method Not Allowed</h2>")
+    else:
+        user = EmailBackEnd.authenticate(request, username=request.POST.get('email'), password=request.POST.get('password'))
+        if user != None:
+            login(request, user)
+            user_type = user.user_type
+            #return HttpResponse("Email: "+request.POST.get('email')+ " Password: "+request.POST.get('password'))
+            if user_type == '1':
+                return redirect('admin_home')
+                
+            elif user_type == '2':
+                # return HttpResponse("Staff Login")
+                return redirect('staff_home')
+                
+            elif user_type == '3':
+                # return HttpResponse("Student Login")
+                return redirect('student_home')
+            else:
+                messages.error(request, "Invalid Login!")
+                return redirect('login')
+        else:
+            messages.error(request, "Invalid Login Credentials!")
+            #return HttpResponseRedirect("/")
+            return redirect('login')
+
+
+
+def get_user_details(request):
+    if request.user != None:
+        return HttpResponse("User: "+request.user.email+" User Type: "+request.user.user_type)
+    else:
+        return HttpResponse("Please Login First")
+
+
+
+def logout_user(request):
+    logout(request)
+    return HttpResponseRedirect('/')
+
+
+def admin_question_view(request):
+    return render(request,'quiz/admin_question.html')
+
+
+def admin_add_question_view(request):
+    questionForm=forms.QuestionForm()
+    if request.method=='POST':
+        questionForm=forms.QuestionForm(request.POST)
+        if questionForm.is_valid():
+            question=questionForm.save(commit=False)
+            course=models.Course.objects.get(id=request.POST.get('courseID'))
+            question.course=course
+            question.save()       
+        else:
+            print("form is invalid")
+        return HttpResponseRedirect('/admin-view-question')
+    return render(request,'quiz/admin_add_question.html',{'questionForm':questionForm})
+
+
+def admin_view_question_view(request):
+    courses= models.Course.objects.all()
+    return render(request,'quiz/admin_view_question.html',{'courses':courses})
+
+
+def view_question_view(request,pk):
+    questions=models.Question.objects.all().filter(course_id=pk)
+    return render(request,'quiz/view_question.html',{'questions':questions})
+
+
+def delete_question_view(request,pk):
+    question=models.Question.objects.get(id=pk)
+    question.delete()
+    return HttpResponseRedirect('/admin-view-question')
